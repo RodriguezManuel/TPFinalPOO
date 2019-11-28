93bc1c0f8080b42b22c051f1f78732e1a98504b5
Gonzalo Arca <goarca@itba.edu.ar>
2019-11-28 18:15:15 -0300

    Agregados Level2, SpecialLevel, SpecialCandyGeneratorCell, modificado campo "grid" de Cell ( private -> protected )

---
+++ src/game/backend/cell/SpecialCandyGeneratorCell.java
@@ -0,0 +1,27 @@
+package game.backend.cell;
+
+import game.backend.element.Candy;
+import game.backend.element.Element;
+import game.backend.level.SpecialLevel;
+
+public abstract class SpecialCandyGeneratorCell extends CandyGeneratorCell {
+    private static final int N = 5; //Determina la frecuencia de generación de caramelos con propiedades especiales (números encima)
+                                    //Mientras mayor el N, menos frecuente es la generación con el pasar de los turnos
+
+    protected SpecialCandyGeneratorCell( SpecialLevel lvl ){
+        super( lvl );
+    }
+
+    @Override
+    public Element getContent(){
+        Element ret = super.getContent();
+        if( !((SpecialLevel)grid).quotaExceeded() && ( 0 == (int)(Math.random() * N) ) ){ //si todavía se pueden generar caramelos especiales
+            Element aux = getSpecialCandy( ret );                                       //y el número generado da 0, se devuelve un caramelo especial
+            ((SpecialLevel)grid).addSpecial();
+        }
+        return ret;
+    }
+
+    protected abstract Element getSpecialCandy( Element base );
+
+}
--- src/game/backend/level/Level2.java
+++ src/game/backend/level/Level2.java
@@ -1,10 +1,61 @@
 package game.backend.level;
 
 import game.backend.GameState;
+import game.backend.element.Candy;
 
-public class Level2 extends Level {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Level2 extends SpecialLevel {
+
+    private static final int MAX_SPECIAL_CANDY = 8;
+    private int spawnedSpecials = 0;
+
+    public Level2(){
+        super(MAX_SPECIAL_CANDY);
+    }
+
+    public void addSpecial( TimeBombCandy candy ){
+        addSpecial();
+        ((Level2State)state()).add( candy );
+    }
+
     @Override
-    protected GameState newState() {
-        return null;
+    public boolean tryMove( int i1, int i2, int j1, int j2 ){
+        if (super.tryMove( i1, i2, j1, j2 )){
+            ((Level2State)state()).decTimers();
+        }
+    }
+
+    @Override
+    protected GameState newState(){
+        return new Level2State( quota );
+    }
+
+    private class Level2State extends SpecialLevelGameState{
+        private List<TimeBombCandy> activeSpecials = new ArrayList<>(quota);
+
+        public Level2State( int candyGoal ){
+            super( candyGoal );
+        }
+
+        @Override
+        public void explodedSpecial( SpecialCandy explodedCandy ){
+            super.explodedSpecial( explodedCandy );
+            activeSpecials.remove( explodedCandy );
+            if( !activeSpecials.isEmpty() ){
+                setCountdown( activeSpecials.get(0).getTimer() );
+            }
+        }
+
+        public void decTimers(){
+            for( TimeBombCandy candy : activeSpecials ){
+                candy.decTimer();
+            }
+        }
+
+        public void add( TimeBombCandy candy ){
+            activeSpecials.add( candy );
+        }
     }
 }
